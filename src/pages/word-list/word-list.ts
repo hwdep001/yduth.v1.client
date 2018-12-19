2c9cacc0fd71a9acb83e69e12983547b767fc13e
howoh <hwdep001@gmail.com>
2018-12-19 17:14:48 +0900

    v.1.0.5 add level fliter

--- src/pages/word-list/word-list.ts
+++ src/pages/word-list/word-list.ts
@@ -1,11 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavParams, Content } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+
+import isEqual from 'lodash/isEqual';
 
 import { CommonProvider } from './../../providers/common-provider';
 import { WordProvider } from './../../providers/word-provider';
 
 import { SearchCondition } from './../../models/sub/SearchCondition';
 import { Word } from './../../models/Word';
+
 
 @IonicPage()
 @Component({
@@ -16,12 +20,20 @@ export class WordListPage {
   @ViewChild(Content) content: Content;
 
   isDataLoaded: boolean = false;
-  words: Array<Word>;
-  sc: SearchCondition;
+
   title: string;
+  sc: SearchCondition;
+  words: Array<Word>;
+  _words: Array<Word>;
+
+  // level filter
+  filterFlag: boolean;
+  levs: Array<number>;
+  selectLevs: Array<number>;
 
   constructor(
     private param: NavParams,
+    private alertCtrl: AlertController,
     private _cmn: CommonProvider,
     private _word: WordProvider
   ) {
@@ -30,9 +42,10 @@ export class WordListPage {
   ionViewDidLoad() {
 
     this.sc = this.param.get("sc");
-
+    
     this.setTitle();
     this.getWords();
+    this.setLevelFilter();
 
     this.isDataLoaded = true;
   }
@@ -51,11 +64,19 @@ export class WordListPage {
 
     this._word.getWordsBySearch(this.sc)
       .then(_words => {
-        this.words = _words;
+
+        this._words = _words;
+
+        if (this.filterFlag) {
+          this.words = this.filterWords();
+        } else {
+          this.words = _words;
+        }
+
         this.content.scrollToTop();
         loader.dismiss();
 
-        this._cmn.Toast.present("bottom", _words.length + " 단어", null);
+        this._cmn.Toast.present("bottom", this.words.length + " 단어", null);
       })
       .catch(err => {
         loader.dismiss();
@@ -64,14 +85,20 @@ export class WordListPage {
       });
   }
 
-  orderQue(): void {
-    this.sc.randomed = false;
-    this.getWords();
-  }
+  setLevelFilter(): void {
+    this.filterFlag = false;
+    this.levs = new Array<number>();
+    this.selectLevs = new Array<number>();
 
-  shuffleQue(): void {
-    this.sc.randomed = true;
-    this.getWords();
+    if (this.sc.levelIdList.length == 0) {
+      this.levs = [2, 1 , 0, -1, -2];
+    } else {
+      this.sc.levelIdList.forEach(levId => {
+        this.levs.push(parseInt(levId + ''));
+      });
+    }
+
+    this.selectLevs = this.levs;
   }
 
   updateLevel($event): void {
@@ -89,6 +116,94 @@ export class WordListPage {
         .catch(() => word.levelId = preLevel);
       word.levelId = level;
     }
+  }
+
+  orderQue(): void {
+    this.sc.randomed = false;
+    this.getWords();
+  }
+
+  shuffleQue(): void {
+    this.sc.randomed = true;
+    this.getWords();
+  }
+
+  fliterQue(): void {
+
+    let inputs = [];
+
+    this.levs.forEach(levId => {
+      inputs.push({
+        type: 'checkbox',
+        label: levId,
+        value: levId,
+        checked: this.selectLevs.indexOf(levId) > -1
+      });
+    });
+
+    this.presentLevelAlert(inputs).then(_selectLevs => { 
+
+      if(_selectLevs.length > 0) {
+
+        const loader = this._cmn.getLoader(null, null);
+        loader.present();
+        
+        this.selectLevs = _selectLevs;
+        this.words = this.filterWords();
+
+        if (isEqual(this.levs,this.selectLevs)) {
+          this.filterFlag = false;
+        } else {
+          this.filterFlag = true;
+        }
+
+        this.content.scrollToTop();
+        loader.dismiss();
+
+        this._cmn.Toast.present("bottom", this.words.length + " 단어", null);
+      }
+
+    }).catch(() => null);
+  }
+
+  filterWords(): Array<Word> {
+
+    let result = new Array<Word>();
+    const words = this._words;
+    const selectLevs = this.selectLevs;
+
+    words.forEach(word => {
+      if (selectLevs.indexOf(word.levelId) > -1) {
+        result.push(word);
+      }
+    });
+
+    return result;
+  }
+
+  private presentLevelAlert(inputs): Promise<Array<number>> {
+    return new Promise<Array<number>>((resolve, reject) => {
+        let check = this.alertCtrl.create({
+            title: 'Filter',
+            message: null,
+            inputs: inputs,
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        reject();
+                    }
+                },
+                {
+                    text: 'Select',
+                    handler: data => {
+                        resolve(data as Array<number>);
+                    }
+                }
+            ]
+        });
+        check.present();
+    });
   }
 
 }
